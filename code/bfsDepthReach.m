c56b6c37c35154bba38f77f4a22770e7eb48a74a
snugghash <snugghash@gmail.com>
2015-11-25 00:10:34 +0530

    Bugfix, now it works as intended.

--- code/bfsDepthReach.m
+++ code/bfsDepthReach.m
@@ -1,13 +1,14 @@
 function [reach] = bfsDepthReach( A, depth, startNode )
 %bfsDepthReach Return the nodes reachable at depth d in a BFS from
 %startNode
+reach = [];
 reach(1,:) = find(A(startNode,:));
 for j = 2:depth
     nextLayer = [];
     for i = reach(j-1,:)
         nextLayer = [nextLayer find(A(i,:))];
     end
-    reach(j,:) = unique(nextLayer);
+    tmp = unique([reach(j-1,:) nextLayer]);
+    reach(j,1:length(tmp)) = tmp;
 end
 end
-
