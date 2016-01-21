4e7d28cd26f9e7c88a7bc7ccdc8f89f539dc4a3d
snugghash <snugghash@gmail.com>
2016-01-21 15:30:43 +0530

    Single objective sensor placement update.
    Refactored sensor placement to getData usage.

--- code/bipAllPossiblePaths.m
+++ code/bipAllPossiblePaths.m
@@ -1,28 +1,15 @@
 %% Get adjacency matrix from pipes
-% Extracts struct from given file
-model = epanet_reader4_extract('bangalore_expanded221.inp');
-% cell2mat(cellfun(@str2num,model.pipes.ni,'un',0).') outputs a proper
-% numeric matrix. The vectors of similar expressions make up the
-% corresponding entries (with opposite sign) in the other triangle.
-adjGraph = sparse([cell2mat(cellfun(@str2num,model.pipes.ni,'un',0).') cell2mat(cellfun(@str2num,model.pipes.nj,'un',0).')],[cell2mat(cellfun(@str2num,model.pipes.nj,'un',0).') cell2mat(cellfun(@str2num,model.pipes.ni,'un',0).')],[ones(1,model.pipes.npipes) -1.*ones(1,model.pipes.npipes)]);
-
-% Set diagonal to 1
-% adjGraph(1 : model.nodes.ntot+1 : model.nodes.ntot*model.nodes.ntot) = 1; 
-
-% Given incidence matrix
-incGraph = adj2inc(adjGraph);
-% Total number of nodes
-nodesNum = model.nodes.ntot;
-% Vulnerable nodes
-vulnerableN = find(strcmp(model.nodes.type,'R'));
+%% Get data from .inp file
+[adjGraph, incGraph, nodesNum, edgesNum, edgeWeights, vulnerableNodes, demandNodes] = getData('bangalore_expanded221.inp');
+vulnerableNodes = [vulnerableNodes 19 32 37 39 53 66];
 
 %% Sensor placement
 % Given vulnerable, find affected for each vulnerable
 % 1 step away affected nodes
 % affectedN = adjGraph(vulnerableN,:)>=1;
 % Find all affected nodes per vulnerable node, each vulnerable node has a row in the A matrix
-A = zeros(model.nodes.no,model.nodes.ntot);
-for i=1:model.nodes.no
+A = zeros(length(vulnerableNodes),nodesNum);
+for i=1:length(vulnerableNodes)
     A(i,graphtraverse(adjGraph,i)) = -1;
 end
 
@@ -30,9 +17,10 @@ end
 f = ones(nodesNum,1)';
 
 %Constraints -Ax >= -b; where (-b)=1
-b = -1.*ones(length(vulnerableN),1);
+b = -1.*ones(length(vulnerableNodes),1);
 
 %% Calling bintprog solver
 options=optimset('Display','iter','NodeDisplayInterval','1','Diagnostics','on');
 [xn,fval,exitflag,output] = bintprog(f,A,b,[],[],[],options);
 sensorNodes = find(xn);
+
