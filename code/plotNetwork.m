0787292ffd1d55795ec4b52588db38546fc9251f
snugghash <snugghash@gmail.com>
2016-05-03 06:11:46 +0530

    Much better plotting.
    Arrows drawn instead of lines, then overdrawn circles.
    TODO: Check if we must edit the arrow.m file for better plotting.

--- code/plotNetwork.m
+++ code/plotNetwork.m
@@ -17,52 +17,51 @@ r = 100; % Radius of drawn nodes
 figure;
 axes;
 hold on;
-linewidth = unique(adjGraph(adjGraph>0));
-for il = 1:length(linewidth)
+lines = unique(adjGraph(adjGraph>0));
+for il = 1:length(lines)
     h = findall(gca, 'type', 'line');
     a=findobj(gcf);
     allaxes=findall(a,'Type','axes');
     alllines=findall(a,'Type','line');
     alltext=findall(a,'Type','text');
-    gplot(adjGraph == linewidth(il), [xcoorID' ycoorID']);
-    hnew = setdiff(findall(gca, 'type', 'line'), h);
-    set(hnew, 'linewidth', 1, 'color', 'black');
+    %gplot(adjGraph == lines(il), [xcoorID' ycoorID']);
+    %hnew = setdiff(findall(gca, 'type', 'line'), h);
+    %set(hnew, 'linewidth', 1, 'color', 'black');
 end
 theta = linspace(0, 2*pi, 500)';
+for ii=1:size(adjGraph,1)
+    jj=find(adjGraph(ii,:));
+    for kk=1:length(jj)
+         arrow([xcoorID(ii) ycoorID(ii)],[xcoorID(jj(kk)),ycoorID(jj(kk))],'Baseangle',80,'Tipangle',17,'Width',0.3,'Length',8);
+    end
+end
 for k=1:length(partitionSource)
-    xc = bsxfun(@plus, r .* cos(theta), xcoorID(partitionSource(k)));
-    yc = bsxfun(@plus, r .* sin(theta), ycoorID(partitionSource(k)));
-    patch(xc, yc, 'r');
+     xc = bsxfun(@plus, r .* cos(theta), xcoorID(partitionSource(k)));
+     yc = bsxfun(@plus, r .* sin(theta), ycoorID(partitionSource(k)));
+     patch(xc, yc, 'r');
 end
 for m=1:length(partitionDemand)
-     %      x1(k)=x(parttion1(k));
-     xc = bsxfun(@plus, r .* cos(theta), xcoorID(partitionDemand(m)));
-     yc = bsxfun(@plus, r .* sin(theta), ycoorID(partitionDemand(m)));
-     patch(xc, yc, 'g');
- end
- for m=1:length(sensorNodes)
-     xc = bsxfun(@plus, r .* cos(theta), xcoorID(sensorNodes(m)));
-     yc = bsxfun(@plus, r .* sin(theta), ycoorID(sensorNodes(m)));
-     patch(xc, yc-150, 'y');
- end
- for k=1:vulnerableNum
-     xc = bsxfun(@plus, r .* cos(theta), xcoorID(vulnerableNodes(k)));
-     yc = bsxfun(@plus, r .* sin(theta), ycoorID(vulnerableNodes(k)));
-     patch(xc+100, yc+100, 'black');
- end
- for k=1:length(demandNodes)
-     xc = bsxfun(@plus, r .* cos(theta), xcoorID(demandNodes(k)));
-     yc = bsxfun(@plus, r .* sin(theta), ycoorID(demandNodes(k)));
-     patch(xc+100, yc+100, 'white');
- end
- hold on
- for ii=1:size(adjGraph,1)
-     jj=find(adjGraph(ii,:));
-     for kk=1:length(jj)
-         %arrow([xcoorID(ii) ycoorID(ii)],[(xcoorID(ii)+xcoorID(jj(kk)))/2,(ycoorID(ii)+ycoorID(jj(kk)))/2],7,'Baseangle',100,'Tipangle',20);
-     end
- end
- %   text(x,y,lbl);
- axis equal;
- axis off;
- hold off;
+    %      x1(k)=x(parttion1(k));
+    xc = bsxfun(@plus, r .* cos(theta), xcoorID(partitionDemand(m)));
+    yc = bsxfun(@plus, r .* sin(theta), ycoorID(partitionDemand(m)));
+    patch(xc, yc, 'g');
+end
+for m=1:length(sensorNodes)
+    xc = bsxfun(@plus, r .* cos(theta), xcoorID(sensorNodes(m)));
+    yc = bsxfun(@plus, r .* sin(theta), ycoorID(sensorNodes(m)));
+    patch(xc, yc-150, 'y');
+end
+for k=1:vulnerableNum
+    xc = bsxfun(@plus, r .* cos(theta), xcoorID(vulnerableNodes(k)));
+    yc = bsxfun(@plus, r .* sin(theta), ycoorID(vulnerableNodes(k)));
+    patch(xc+100, yc+100, 'black');
+end
+for k=1:length(demandNodes)
+   xc = bsxfun(@plus, r .* cos(theta), xcoorID(demandNodes(k)));
+   yc = bsxfun(@plus, r .* sin(theta), ycoorID(demandNodes(k)));
+   patch(xc+100, yc+100, 'white');
+end
+%   text(x,y,lbl);
+axis equal;
+axis off;
+hold off;
